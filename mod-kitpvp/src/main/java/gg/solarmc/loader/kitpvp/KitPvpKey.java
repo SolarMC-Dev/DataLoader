0aa6467b2dc1b89928e92ea3eeb40581ca8ac209
A248 <anandebeh@gmail.com>
2021-01-18 16:15:38 -0500

    Add some missing license headers

--- mod-kitpvp/src/main/java/gg/solarmc/loader/kitpvp/KitPvpKey.java
+++ mod-kitpvp/src/main/java/gg/solarmc/loader/kitpvp/KitPvpKey.java
@@ -1,12 +1,27 @@
+/*
+ * DataLoader
+ * Copyright © 2021 SolarMC Developers
+ *
+ * DataLoader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * DataLoader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DataLoader. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Affero General Public License.
+ */
+
 package gg.solarmc.loader.kitpvp;
 
 import gg.solarmc.loader.data.DataKey;
 import gg.solarmc.loader.data.DataKeyInitializationContext;
 import gg.solarmc.loader.data.DataLoader;
-import gg.solarmc.loader.data.DataManager;
-import space.arim.omnibus.util.concurrent.FactoryOfTheFuture;
-
-import java.nio.file.Path;
 
 public class KitPvpKey implements DataKey<KitPvp, KitPvpManager> {
 

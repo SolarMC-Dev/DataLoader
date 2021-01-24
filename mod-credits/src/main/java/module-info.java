c1e1c95a50a4aebb217ac6376e89e95547b59135
A248 <anandebeh@gmail.com>
2021-01-24 14:09:42 -0500

    Add new and fix existing module-info files

---
+++ mod-credits/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module gg.solarmc.loader.credits {
+	exports gg.solarmc.loader.credits;
+
+	requires transitive gg.solarmc.loader;
+	requires gg.solarmc.loader.impl;
+	requires space.arim.dazzleconf.ext.snakeyaml;
+}

e8d7b8f534e6b490d5b9b9b15a168d140b6f4747
Auriium <ekeeekee148@gmail.com>
2021-01-25 13:40:46 -0700

    clans-working init and start on schema

--- core-schema/src/main/resources/sql-schema/V1__Create_initial_schema.sql
+++ core-schema/src/main/resources/sql-schema/V1__Create_initial_schema.sql
@@ -60,3 +60,36 @@ CREATE TABLE friends_friended_users (
   FOREIGN KEY (friend_user_id) REFERENCES user_ids (id) ON DELETE CASCADE,
   PRIMARY KEY (user_id, friend_user_id)
 );
+
+CREATE TABLE clans_clan_info (
+  clan_id INT NOT NULL AUTO_INCREMENT UNIQUE,
+  clan_name VARCHAR(100) NOT NULL,
+  clan_kills INT NOT NULL,
+  clan_deaths INT NOT NULL,
+  clan_assists INT NOT NULL,
+  PRIMARY KEY (clan_id)
+);
+
+CREATE TABLE clans_clan_membership (
+  clan_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (clan_id) REFERENCES clans_clan_info (clan_id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES user_ids (id) ON DELETE CASCADE,
+  PRIMARY KEY (clan_id,user_id)
+);
+
+CREATE TABLE clans_clan_alliances (
+  clan_id INT NOT NULL,
+  ally_id INT NOT NULL,
+  FOREIGN KEY (clan_id) REFERENCES clans_clan_info (clan_id) ON DELETE CASCADE,
+  FOREIGN KEY (ally_id) REFERENCES clans_clan_info (clan_id) ON DELETE CASCADE,
+  PRIMARY KEY (clan_id,ally_id)
+);
+
+CREATE TABLE clans_clan_enemies (
+  clan_id INT NOT NULL,
+  enemy_id INT NOT NULL,
+  FOREIGN KEY (clan_id) REFERENCES clans_clan_info (clan_id) ON DELETE CASCADE,
+  FOREIGN KEY (enemy_id) REFERENCES clans_clan_info (clan_id) ON DELETE CASCADE,
+  PRIMARY KEY (clan_id,enemy_id)
+);

dc5c8ef9072257901ef2d2b17a2dc6f81f969b7e
A248 <anandebeh@gmail.com>
2021-01-29 09:05:40 -0500

    Add providers for DataKeySpi

--- mod-kitpvp/src/main/java/module-info.java
+++ mod-kitpvp/src/main/java/module-info.java
@@ -4,4 +4,6 @@ module gg.solarmc.loader.kitpvp {
 	requires transitive gg.solarmc.loader;
 	requires gg.solarmc.loader.impl;
 	requires com.github.benmanes.caffeine;
+
+	provides gg.solarmc.loader.data.DataKeySpi with gg.solarmc.loader.kitpvp.KitPvpKeySpi;
 }

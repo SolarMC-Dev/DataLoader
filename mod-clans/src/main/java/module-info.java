81177b650a81e19546cf54aa674a3c71a50a7fc9
Auriium <ekeeekee148@gmail.com>
2021-02-01 19:48:18 -0700

    no longer build failing *sigh*

--- mod-clans/src/main/java/module-info.java
+++ mod-clans/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module gg.solarmc.loader.clans {
 
     requires transitive gg.solarmc.loader;
     requires gg.solarmc.loader.impl;
+    requires com.github.benmanes.caffeine;
 
     provides gg.solarmc.loader.data.DataKeySpi with gg.solarmc.loader.clans.ClansKeySpi;
 }

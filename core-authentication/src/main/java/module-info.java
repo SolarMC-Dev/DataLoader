02722d03fb3902e6b306b268a87cdb4e9be18344
A248 <anandebeh@gmail.com>
2021-02-06 19:46:03 -0500

    Fix compilation of module-info

--- core-authentication/src/main/java/module-info.java
+++ core-authentication/src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module gg.solarmc.loader.authentication {
 	exports gg.solarmc.loader.authentication;
 
-	requires transitive gg.solarmc.loader;
 	requires gg.solarmc.loader.impl;
 	requires de.mkammerer.argon2;
-
-	provides gg.solarmc.loader.data.DataKeySpi with gg.solarmc.loader.authentication.AuthKeySpi;
 }

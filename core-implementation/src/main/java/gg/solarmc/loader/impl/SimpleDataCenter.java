0e2d8efd68b3a816e759964760ac095a7ab0c9c6
A248 <anandebeh@gmail.com>
2021-02-16 16:25:31 -0500

    Add SimpleDataCenter

---
+++ core-implementation/src/main/java/gg/solarmc/loader/impl/SimpleDataCenter.java
@@ -0,0 +1,114 @@
+/*
+ * DataLoader
+ * Copyright © 2021 SolarMC Developers
+ *
+ * DataLoader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * DataLoader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DataLoader. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Affero General Public License.
+ */
+
+package gg.solarmc.loader.impl;
+
+import gg.solarmc.loader.DataCenter;
+import gg.solarmc.loader.OnlineSolarPlayer;
+import gg.solarmc.loader.SolarPlayer;
+import gg.solarmc.loader.data.DataKey;
+import gg.solarmc.loader.data.DataManager;
+import org.jooq.DSLContext;
+import org.jooq.Record2;
+import space.arim.omnibus.util.UUIDUtil;
+import space.arim.omnibus.util.concurrent.CentralisedFuture;
+import space.arim.omnibus.util.concurrent.FactoryOfTheFuture;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static gg.solarmc.loader.schema.tables.LatestNames.LATEST_NAMES;
+
+/**
+ * A {@link DataCenter} which uses a {@link PlayerTracker}
+ *
+ */
+public final class SimpleDataCenter implements DataCenter {
+
+	private final FactoryOfTheFuture futuresFactory;
+	private final Icarus icarus;
+	private final PlayerTracker playerTracker;
+
+	public SimpleDataCenter(FactoryOfTheFuture futuresFactory, Icarus icarus, PlayerTracker playerTracker) {
+		this.futuresFactory = futuresFactory;
+		this.icarus = icarus;
+		this.playerTracker = playerTracker;
+	}
+
+	@Override
+	public CentralisedFuture<?> runTransact(DataCenter.TransactionRunner runner) {
+		return icarus.transactionSource().runTransact(runner);
+	}
+
+	@Override
+	public <R> CentralisedFuture<R> transact(DataCenter.TransactionActor<R> actor) {
+		return icarus.transactionSource().transact(actor);
+	}
+
+	@Override
+	public <M extends DataManager> M getDataManager(DataKey<?, ?, M> key) {
+		return icarus.dataManagement().getDataManager(key);
+	}
+
+	@Override
+	public CentralisedFuture<Optional<SolarPlayer>> lookupPlayer(String name) {
+		Optional<OnlineSolarPlayer> instantPlayer = playerTracker.getOnlinePlayerForName(name);
+		if (instantPlayer.isPresent()) {
+			return futuresFactory.completedFuture(upcastOptional(instantPlayer));
+		}
+		return transact((transaction) -> {
+			Record2<Integer, byte[]> latestNamesRecord = transaction.getProperty(DSLContext.class)
+					.select(LATEST_NAMES.USER_ID, LATEST_NAMES.UUID)
+					.from(LATEST_NAMES)
+					.where(LATEST_NAMES.USERNAME.eq(name))
+					.fetchOne();
+			if (latestNamesRecord == null) {
+				return Optional.empty();
+			}
+			int userId = latestNamesRecord.value1();
+			UUID mcUuid = UUIDUtil.fromByteArray(latestNamesRecord.value2());
+			return Optional.of(icarus.loginHandler().createOfflineUser(userId, mcUuid));
+		});
+	}
+
+	@Override
+	public CentralisedFuture<Optional<SolarPlayer>> lookupPlayer(UUID uuid) {
+		Optional<OnlineSolarPlayer> instantPlayer = playerTracker.getOnlinePlayerForUuid(uuid);
+		if (instantPlayer.isPresent()) {
+			return futuresFactory.completedFuture(upcastOptional(instantPlayer));
+		}
+		return transact((transaction) -> {
+			Integer userId = transaction.getProperty(DSLContext.class)
+					.select(LATEST_NAMES.USER_ID)
+					.from(LATEST_NAMES)
+					.where(LATEST_NAMES.UUID.eq(UUIDUtil.toByteArray(uuid)))
+					.fetchOne(LATEST_NAMES.USER_ID);
+			if (userId == null) {
+				return Optional.empty();
+			}
+			return Optional.of(icarus.loginHandler().createOfflineUser(userId, uuid));
+		});
+	}
+
+	@SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "unchecked"})
+	private static <T extends R, R> Optional<R> upcastOptional(Optional<T> optional) {
+		return (Optional<R>) optional;
+	}
+
+}
--- core-schema/src/main/resources/sql-schema/V3__Identification_continued.sql
+++ core-schema/src/main/resources/sql-schema/V3__Identification_continued.sql
@@ -1,18 +1,18 @@
 
 CREATE VIEW name_history AS
-  SELECT user_ids.id user_id, libertybans_names.name username, libertybans_names.updated updated
+  SELECT user_ids.id user_id, user_ids.uuid uuid, libertybans_names.name username, libertybans_names.updated updated
   FROM libertybans_names INNER JOIN user_ids
   ON libertybans_names.uuid = user_ids.uuid;
 
 CREATE VIEW address_history AS
-  SELECT user_ids.id user_id, libertybans_addresses.address address, libertybans_addresses.updated updated
+  SELECT user_ids.id user_id, user_ids.uuid uuid, libertybans_addresses.address address, libertybans_addresses.updated updated
   FROM libertybans_addresses INNER JOIN user_ids
   ON libertybans_addresses.uuid = user_ids.uuid;
 
 -- Exclusive outer join to return the most recent name of each player
 
 CREATE VIEW latest_names AS
-  SELECT nh1.user_id, nh1.username, nh1.updated
+  SELECT nh1.user_id, nh1.uuid, nh1.username, nh1.updated
   FROM name_history nh1
   LEFT JOIN name_history nh2
   ON nh1.user_id = nh2.user_id
